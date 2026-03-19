4e95159a892733e3dc7bc483421ef2793df6a0fc
Konstantin Kurshin <ekurshin@gmail.com>
2026-03-19 23:02:39 +0300

    fix  playwright_registration_button

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -22,4 +22,3 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     expect(registration_button).to_be_enabled()
     
-    page.wait_for_timeout(5000) 
