acd54c216dae61180f38176fa24604bcf8592a16
Konstantin Kurshin <ekurshin@gmail.com>
2026-03-27 14:49:18 +0300

    fix chromium_page, initialize_browser_state fixturess for conftest.py and fix test_courses

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,25 +1,49 @@
+# import pytest
+# from playwright.sync_api import expect, Page
+
+
+# @pytest.mark.courses
+# @pytest.mark.regression
+# def test_empty_courses_list(chromium_page_with_state):
+#     page = chromium_page_with_state
+
+#     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+#     courses_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
+#     expect(courses_toolbar_title).to_be_visible()
+#     expect(courses_toolbar_title).to_have_text('Courses')
+    
+#     courses_list_empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
+#     expect(courses_list_empty_icon).to_be_visible()
+    
+#     courses_list_empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
+#     expect(courses_list_empty_title).to_be_visible()
+#     expect(courses_list_empty_title).to_have_text('There is no results')
+    
+#     courses_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+#     expect(courses_description_text).to_be_visible()
+#     expect(courses_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
 import pytest
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Page
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-    page = chromium_page_with_state
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-    courses_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_toolbar_title).to_be_visible()
-    expect(courses_toolbar_title).to_have_text('Courses')
-    
-    courses_list_empty_icon = page.get_by_test_id('courses-list-empty-view-icon')
-    expect(courses_list_empty_icon).to_be_visible()
-    
-    courses_list_empty_title = page.get_by_test_id('courses-list-empty-view-title-text')
-    expect(courses_list_empty_title).to_be_visible()
-    expect(courses_list_empty_title).to_have_text('There is no results')
-    
-    courses_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
-    expect(courses_description_text).to_be_visible()
-    expect(courses_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
