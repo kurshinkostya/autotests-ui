3409fba6d29dcb1417348a4735259c5ee492c077
Konstantin Kurshin <ekurshin@gmail.com>
2026-04-10 23:56:11 +0300

    fix login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,11 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from pages.base_page import BasePage
 from components.authentication.login_form_component import LoginFormComponent
+
+from elements.button import Button
+from elements.link import Link
+from elements.text import Text
 
 
 class LoginPage(BasePage):
@@ -10,11 +14,17 @@ class LoginPage(BasePage):
 
         self.login_form = LoginFormComponent(page)
 
-        self.login_button = page.get_by_test_id('login-page-login-button')
-        self.registration_link = page.get_by_test_id('login-page-registration-link')
-        self.wrong_email_or_password_alert = page.get_by_test_id(
-            'login-page-wrong-email-or-password-alert'
+        self.login_button = Button(page, 'login-page-login-button', 'Login button')
+        self.registration_link = Link(page, 'login-page-registration-link', 'Registration link')
+        self.wrong_email_or_password_alert = Text(
+            page,
+            'login-page-wrong-email-or-password-alert',
+            'Wrong email or password alert'
         )
+
+    def login(self, email: str, password: str):
+        self.login_form.fill(email, password)
+        self.login_button.click()
 
     def click_login_button(self):
         self.login_button.click()
@@ -23,11 +33,5 @@ class LoginPage(BasePage):
         self.registration_link.click()
 
     def check_visible_wrong_email_or_password_alert(self):
-        expect(self.wrong_email_or_password_alert).to_be_visible()
-        expect(self.wrong_email_or_password_alert).to_have_text(
-            "Wrong email or password"
-        )
-
-    def login(self, email: str, password: str):
-        self.login_form.fill(email, password)
-        self.click_login_button()
+        self.wrong_email_or_password_alert.check_visible()
+        self.wrong_email_or_password_alert.check_have_text('Wrong email or password')
