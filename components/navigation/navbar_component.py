d3d5084449658ef291acdcff876b109be580583c
Konstantin Kurshin <ekurshin@gmail.com>
2026-04-11 00:09:15 +0300

    add pagefactory elements for pages

--- components/navigation/navbar_component.py
+++ components/navigation/navbar_component.py
@@ -4,22 +4,7 @@ from components.base_component import BaseComponent
 
 from elements.text import Text
 
-
-# class NavbarComponent(BaseComponent):
-#     def __init__(self, page: Page):
-#         super().__init__(page)
         
-#         self.app_title = Text(page, 'navigation-navbar-app-title-text', 'App title')
-#         self.welcome_title = Text(page, 'navigation-navbar-welcome-title-text', 'Welcome title')
-        
-#     def check_visible(self, username: str):
-#          self.app_title.check_visible()
-#          self.app_title.check_have_text('UI Course ')
-         
-#          self.welcome_title.check_visible()
-#          self.app_title.check_have_text(f'Welcome, {username}!')
-         
-         
 class NavbarComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -8,26 +8,6 @@ from elements.text import Text
 from elements.button import Button
 
 
-# class SidebarListItemComponent(BaseComponent):
-#     def __init__(self, page: Page, identifier: str):
-#         super().__init__(page)
-        
-#         self.icon = Icon(page, f'{identifier}-drawer-list-item-icon', 'Icon')
-#         self.title = Text(page, (f'{identifier}-drawer-list-item-title-text'), 'Title')
-#         self.button = Button(page, f'{identifier}-drawer-list-item-button', 'Button')
-        
-#     def check_visible(self, title: str):
-#         self.icon.check_visible()
-        
-#         self.title.to_be_visible()
-#         self.title.check_have_text(title)
-        
-#         self.to_be_visible()
-        
-#     def navigate(self, expected_url: Pattern[str]):
-#         self.button.click()
-#         self.check_current_url(expected_url)
-
 class SidebarListItemComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
