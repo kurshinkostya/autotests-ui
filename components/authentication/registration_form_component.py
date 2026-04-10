2c3fd0e5859d255ad40452731cca320b53d594f7
Konstantin Kurshin <ekurshin@gmail.com>
2026-04-11 00:01:16 +0300

    add elements for components

--- components/authentication/registration_form_component.py
+++ components/authentication/registration_form_component.py
@@ -1,16 +1,47 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
+
+# class RegistrationFormComponent(BaseComponent):
+#     def __init__(self, page: Page):
+#         super().__init__(page)
+        
+#         self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+#         self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+#         self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        
+#         self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+#     def fill(self, email: str, username: str, password: str):
+#         self.email_input.fill(email)
+#         self.username_input.fill(username)
+#         self.password_input.fill(password)
+
+#     def check_visible(self, email: str, username: str, password: str):
+#         expect(self.email_input).to_be_visible()
+#         expect(self.username_input).to_be_visible()
+#         expect(self.password_input).to_be_visible()
+        
+#         expect(self.email_input).to_have_value(email)
+#         expect(self.username_input).to_have_value(username)
+#         expect(self.password_input).to_have_value(password)
+
+#     def submit(self):
+#         self.registration_button.click()
+        
+from elements.input import Input
+from elements.button import Button
+
 
 class RegistrationFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
-        
-        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
+
+        self.email_input = Input(page, 'registration-form-email-input', 'Email')
+        self.username_input = Input(page, 'registration-form-username-input', 'Username')
+        self.password_input = Input(page, 'registration-form-password-input', 'Password')
+
+        self.registration_button = Button(page, 'registration-page-registration-button', 'Register button')
 
     def fill(self, email: str, username: str, password: str):
         self.email_input.fill(email)
@@ -18,13 +49,14 @@ class RegistrationFormComponent(BaseComponent):
         self.password_input.fill(password)
 
     def check_visible(self, email: str, username: str, password: str):
-        expect(self.email_input).to_be_visible()
-        expect(self.username_input).to_be_visible()
-        expect(self.password_input).to_be_visible()
-        
-        expect(self.email_input).to_have_value(email)
-        expect(self.username_input).to_have_value(username)
-        expect(self.password_input).to_have_value(password)
+        self.email_input.check_visible()
+        self.email_input.check_have_value(email)
+
+        self.username_input.check_visible()
+        self.username_input.check_have_value(username)
+
+        self.password_input.check_visible()
+        self.password_input.check_have_value(password)
 
     def submit(self):
         self.registration_button.click()
