a33fd34b2d51801ae83b345c3a092d65e2556dc9
Konstantin Kurshin <ekurshin@gmail.com>
2026-04-18 12:40:24 +0300

    add allure tags, epics, stories, severenity and title for tests

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,13 +1,29 @@
+import allure
 import pytest
+
 from playwright.sync_api import Page
 
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from allure_commons.types import Severity
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit(
             "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
@@ -49,6 +65,8 @@ class TestCourses:
             estimated_time="2 weeks"
         )
     
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit(
             "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
@@ -60,7 +78,9 @@ class TestCourses:
 
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
-   
+    
+    @allure.title("Edit course")
+    @allure.severity(Severity.NORMAL)
     def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
 
