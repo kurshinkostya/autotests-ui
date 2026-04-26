755b72218d4fba93d6d9768619fba29ecd46fe88
Konstantin Kurshin <ekurshin@gmail.com>
2026-04-26 21:32:54 +0300

    rewrite fixtures/browsers

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,16 +1,17 @@
 import pytest
+
+from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
-
-
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    
+    
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -27,10 +28,10 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    yield context.new_page()
-    browser.close()
-
-    
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
+    yield from initialize_playwright_page(
+        playwright, 
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+        )
+   
